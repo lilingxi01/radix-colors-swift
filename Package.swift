0a6bc2022b767f47a4c2a7b46c59d778f9fafb11
Lingxi Li <lilingxi01@gmail.com>
2023-01-15 21:53:26 -0500

    chore: remove redundant comments.

--- Package.swift
+++ Package.swift
@@ -7,19 +7,13 @@ let package = Package(
     name: "RadixColors",
     platforms: [.iOS(.v15), .macOS(.v10_15)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "RadixColors",
             targets: ["RadixColors"]
         ),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RadixColors",
             dependencies: []
